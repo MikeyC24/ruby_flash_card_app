4dbefa5709aff8a4095c0eba70f6842ce996ec62
mikeyc24 <mgchapman24@gmail.com>
2017-06-06 10:07:36 -0400

    set up inital layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	#seting up title for each page
+	def full_title(page_title = '')
+		base_title = "Ruby Flash Card App"
+		if page_title.empty?
+			base_title
+		else
+			page_title
+		end
+	end
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,18 +2,27 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "Ruby Flash Card App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help"
   end
 
   test "should get about page" do 
   	get static_pages_about_url
   	assert_response :success
+    assert_select "title", "About"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact"
   end
 
 end
