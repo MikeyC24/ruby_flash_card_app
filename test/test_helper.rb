3a0c38aed57f249ce0f843918d13a275841bb207
mikeyc24 <mgchapman24@gmail.com>
2017-06-07 07:48:06 -0400

    can now log in and out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
@@ -5,5 +6,9 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  #reutrns true if user is logged in
+  def is_logged_in?
+  	!session[:user_id].nil?
+  end
+
 end
